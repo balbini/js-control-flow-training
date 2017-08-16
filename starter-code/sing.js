d8e684dc681ddbef533dfefd4e5621db67d9aad4
balbini <balbini91@gmail.com>
2017-08-15 22:27:29 -0700

    removed unneeded comments

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -6,7 +6,5 @@ let bottlesOfBeer = function(){
   console.log(bottles + " bottles of beer on the wall!");
   console.log(bottles + " bottles of beer!");
   console.log("Take one down, pass it around, " + bottles-- + " bottles of beer on the wall");
-  //bottles--;
-  //console.log(bottles + " bottles of beer on the wall!");
 }};
 bottlesOfBeer();
